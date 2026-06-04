786d13d2517090dbf340bf6f839eefa2263c3c96
Bastien Labelle <hello@bastienlabelle.fr>
2026-06-04 09:48:16 +0200

    New menu

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -145,31 +145,37 @@ const menuItems = [
     to: '/',
     label: 'Dashboard',
     exact: true,
-    icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6',
+    icon: 'M4 13h6V4H4v9zm0 7h6v-5H4v5zm10 0h6V9h-6v11zm0-18v7h6V2h-6z',
   },
   {
     to: '/clients',
     label: 'Clients',
     exact: false,
-    icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0z',
+    icon: 'M9 12a3 3 0 100-6 3 3 0 000 6zm6 0a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM2 20a7 7 0 0114 0H2zm12 0a5.5 5.5 0 015-5.5V20h-5z',
   },
   {
     to: '/quotes',
     label: 'Devis',
     exact: false,
-    icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
+    icon: 'M7 3h10a2 2 0 012 2v14l-7-3-7 3V5a2 2 0 012-2zm2 5h6v2H9V8zm0 4h6v2H9v-2z',
   },
   {
     to: '/invoices',
     label: 'Factures',
     exact: false,
-    icon: 'M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z',
+    icon: 'M7 2h8l5 5v15a2 2 0 01-2 2H7a2 2 0 01-2-2V4a2 2 0 012-2zm8 1v5h5M9 12h6M9 16h6',
   },
   {
     to: '/payments',
     label: 'Paiements',
     exact: false,
-    icon: 'M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z',
+    icon: 'M3 7a3 3 0 013-3h12a3 3 0 013 3v10a3 3 0 01-3 3H6a3 3 0 01-3-3V7zm2 0v2h14V7H5zm0 6h5v2H5v-2z',
+  },
+  {
+    to: '/declarations',
+    label: 'Déclarations',
+    exact: false,
+    icon: 'M6 2h9l5 5v15a2 2 0 01-2 2H6a2 2 0 01-2-2V4a2 2 0 012-2zm3 7h6v2H9V9zm0 4h6v2H9v-2zm0 4h4v2H9v-2z',
   },
 ]
 
